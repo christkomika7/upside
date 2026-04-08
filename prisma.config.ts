87328e42dcaccc044027c66991452298a5698d56
christkomika7 <projects.zua@gmail.com>
2026-04-08 05:12:42 +0100

    Correction bug

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,12 +1,14 @@
-import { defineConfig, env } from "prisma/config";
+import "dotenv/config";
+import type { PrismaConfig } from "prisma";
+import { env } from "prisma/config";
 
-export default defineConfig({
+export default {
   schema: "prisma/schema.prisma",
   migrations: {
     path: "prisma/migrations",
-    seed: `bun ./prisma/seed.ts`,
+    seed: "tsx prisma/seed.ts",
   },
   datasource: {
     url: env("DATABASE_URL"),
   },
-});
+} satisfies PrismaConfig;
