3860f06e99623b01b9c7e009f5a6a3246a5d325b
christkomika7 <projects.zua@gmail.com>
2026-04-08 14:23:22 +0100

    Correction bug

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,16 +1,10 @@
-import { PrismaClient } from '../app/generated/prisma'
-import { withAccelerate } from '@prisma/extension-accelerate'
+import "dotenv/config";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "../generated/prisma/client";
 
-const globalForPrisma = global as unknown as {
-    prisma: ReturnType<typeof createPrismaClient>
-}
+const connectionString = `${process.env.DATABASE_URL}`;
 
-function createPrismaClient() {
-    return new PrismaClient().$extends(withAccelerate())
-}
+const adapter = new PrismaPg({ connectionString });
+const prisma = new PrismaClient({ adapter });
 
-const prisma = globalForPrisma.prisma ?? createPrismaClient()
-
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
-
-export default prisma
+export { prisma };
