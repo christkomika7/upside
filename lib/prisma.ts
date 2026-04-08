759ce5c561b49c3a106b0b99219ea524d306ecc4
christkomika7 <projects.zua@gmail.com>
2026-04-08 13:57:05 +0100

    Correction bug

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,19 +1,24 @@
-import { PrismaClient } from '@/app/generated/prisma'
+import { PrismaClient } from '../app/generated/prisma'
 import { withAccelerate } from '@prisma/extension-accelerate'
 
 const globalForPrisma = global as unknown as {
-    prisma: PrismaClient
+    prisma: ReturnType<typeof createPrismaClient>
 }
 
 function createPrismaClient() {
-    return new PrismaClient().$extends(withAccelerate())
+    const url = process.env.DATABASE_URL
+    if (!url) {
+        throw new Error('DATABASE_URL is not defined')
+    }
+    return new PrismaClient({
+        datasources: {
+            db: { url },
+        },
+    }).$extends(withAccelerate())
 }
 
-const prisma =
-    globalForPrisma.prisma ?? createPrismaClient()
+const prisma = globalForPrisma.prisma ?? createPrismaClient()
 
-if (process.env.NODE_ENV !== 'production') {
-    globalForPrisma.prisma = prisma
-}
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
 
 export default prisma
