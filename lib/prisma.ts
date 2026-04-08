5187648d08d2f085a2f9ee755cfa5fdca914ce1a
christkomika7 <projects.zua@gmail.com>
2026-04-08 05:44:21 +0100

    Correction bug

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -5,14 +5,8 @@ const globalForPrisma = global as unknown as {
     prisma: PrismaClient
 }
 
-const prisma =
-    globalForPrisma.prisma ||
-    new PrismaClient({
-        datasourceUrl: process.env.DATABASE_URL,
-    }).$extends(withAccelerate())
+const prisma = globalForPrisma.prisma || new PrismaClient().$extends(withAccelerate())
 
-if (process.env.NODE_ENV !== 'production') {
-    globalForPrisma.prisma = prisma
-}
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
 
 export default prisma
