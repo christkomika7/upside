09ad1b9ad2452c58eafcc38dca7bb31fa07ed51a
christkomika7 <projects.zua@gmail.com>
2026-04-08 12:20:58 +0100

    Correction bug

--- app/api/option/list/route.ts
+++ app/api/option/list/route.ts
@@ -1,10 +1,11 @@
 import prisma from "@/lib/prisma";
 import { NextResponse, type NextRequest } from "next/server";
+import { Options } from "@/app/generated/prisma";
 
 export async function GET(req: NextRequest) {
     const datas = await prisma.options.findMany();
     const baseUrl = new URL(req.url).origin;
-    const processedData = datas.length > 0 ? datas.map(option => {
+    const processedData = datas.length > 0 ? datas.map((option: Options) => {
         const imageUrl = option.icon
             ? option.icon.startsWith('http')
                 ? option.icon
