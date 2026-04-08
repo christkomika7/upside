10e7f06a4fc055f40eb0e03418ffacad1ad6501a
christkomika7 <projects.zua@gmail.com>
2026-04-08 10:51:53 +0100

    Correction bug

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -2,10 +2,20 @@ import { PrismaClient } from '../app/generated/prisma'
 import { withAccelerate } from '@prisma/extension-accelerate'
 
 const globalForPrisma = global as unknown as {
-    prisma: PrismaClient
+    prisma: ReturnType<typeof createPrismaClient>
 }
 
-const prisma = globalForPrisma.prisma || new PrismaClient().$extends(withAccelerate())
+function createPrismaClient() {
+    return new PrismaClient({
+        datasources: {
+            db: {
+                url: process.env.DATABASE_URL!,
+            },
+        },
+    }).$extends(withAccelerate())
+}
+
+const prisma = globalForPrisma.prisma ?? createPrismaClient()
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
 
